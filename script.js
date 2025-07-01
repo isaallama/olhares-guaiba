01cc6fa8fbfea1cd188f3da8c93e70f543b5a642
Isadora Allama <isadoraallama@gmail.com>
2025-07-01 14:55:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openForm(category) {
     if (category === 'estudantil') {
         // Abrir formulário do Google para estudantes
-        window.open('https://forms.gle/ti-form-id-student', '_blank');
+        window.open('https://forms.gle/dHrk63hT52ewzybp7', '_blank');
     } else if (category === 'comunidade') {
         // Abrir formulário do Google para comunidade
-        window.open('https://forms.gle/ti-form-id-comunity', '_blank');
+        window.open('https://forms.gle/SKrXBGMHdzPPGxbV9', '_blank');
     }
 }
